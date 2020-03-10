2814255efd36ce1760f218d2cdb66a7c27951327
Remy Sharp <remy@remysharp.com>
2020-03-10 00:12:54 +0000

    feat: working sprite conversion

---
+++ public/colours/lib/save.js
@@ -0,0 +1,13 @@
+export default (function() {
+  var a = document.createElement("a");
+  document.body.appendChild(a);
+  a.style = "display: none";
+  return function(data, fileName) {
+    const blob = new Blob([data], { type: "octet/stream" });
+    const url = URL.createObjectURL(blob);
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    URL.revokeObjectURL(url);
+  };
+})();
