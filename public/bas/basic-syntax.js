e9c739631dbf6ad1412d0a35a6d5786bfb2c0243
Remy Sharp <remy@remysharp.com>
2020-03-15 22:10:38 +0000

    build tweak

--- public/bas/basic-syntax.js
+++ public/bas/basic-syntax.js
@@ -1,5 +1,5 @@
 import CodeMirror from './lib/cm.js';
-import 'CodeMirror/addon/mode/simple';
+import 'CodeMirror/addon/mode/simple.js';
 import codes from './codes.js';
 
 const keywords = Object.values(codes).filter(
