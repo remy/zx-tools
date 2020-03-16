1dce15d20994fa5db96aee2027a32afab583aa3a
Remy Sharp <remy@remysharp.com>
2020-03-16 10:30:33 +0000

    new build

--- dist/sprites.e070f643.js
+++ dist/sprites.e070f643.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51185" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51887" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
