421cc802831967d60608e417d3d9317dc38df1ff
Remy Sharp <remy@remysharp.com>
2020-05-02 13:05:32 +0100

    functions in now

---
+++ functions/api/bas2txt.js
@@ -0,0 +1,40 @@
+/* eslint-env node */
+const { file2txt } = require('txt2bas');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+module.exports = (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method Not Allowed (please POST data=+3dos binary only)',
+    });
+  }
+
+  upload.single('file')(req, res, (err) => {
+    if (err) {
+      return res.status(400).send({
+        error: err.message,
+      });
+    }
+    if (!req.file) {
+      return res.status(400).send({
+        error:
+          'Request body must contain `file` POST with binary file contents',
+      });
+    }
+    try {
+      const src = Uint8Array.from(req.file.buffer);
+
+      const body = file2txt(src);
+
+      res.status(200);
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Content-Type', 'text/plain');
+      res.send(body);
+    } catch (err) {
+      console.log(err.stack); // output to netlify function log
+      res.status(500).json({ error: err.message });
+    }
+  });
+};
---
+++ functions/api/txt2bas.js
@@ -0,0 +1,46 @@
+/* eslint-env node */
+const { file2bas } = require('txt2bas');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+module.exports = (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({
+      error: 'Method Not Allowed (please POST data=+3dos binary only)',
+    });
+  }
+
+  upload.single('file')(req, res, (err) => {
+    if (err) {
+      return res.status(400).send({
+        error: err.message,
+      });
+    }
+
+    try {
+      let src = '';
+      try {
+        if (req.file) {
+          src = req.file.buffer;
+        } else {
+          src = JSON.parse(req.body).text;
+        }
+      } catch (e) {
+        if (req.headers['content-type'].toLowerCase().includes('text/plain')) {
+          src = req.body;
+        } else {
+          src = req.body.text;
+        }
+      }
+
+      const body = file2bas(src);
+
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.send(Buffer.from(body), 'binary');
+    } catch (err) {
+      console.log(err); // output to netlify function log
+      res.status(500).json({ error: err.message });
+    }
+  });
+};
