6ba7aa94c873ac11bb6bec7017bdaab702195dbf
Remy Sharp <remy@remysharp.com>
2020-05-18 16:38:59 +0100

    Support github import

--- public/lib/gist.js
+++ public/lib/gist.js
@@ -1,14 +1,35 @@
 export async function loadGist() {
   const id = getGistId(window.location.toString());
+
+  if (id.includes('github.com')) {
+    return loadGitHub(id);
+  }
+
   const res = await fetch(`https://api.github.com/gists/${id}`);
   const json = await res.json();
 
   const file = Object.keys(json.files)
-    .map(key => json.files[key])
-    .find(_ => _.filename.toLowerCase().endsWith('.bas'));
+    .map((key) => json.files[key])
+    .find((_) => _.filename.toLowerCase().endsWith('.bas'));
   if (file) {
     return file.content;
   }
+}
+
+async function loadGitHub(url) {
+  const match = url.match(/github\.com\/(.*?)\/(.*?)\/blob\/(.*?)\/(.*)/i);
+
+  if (!match) return;
+  match.shift();
+  const [owner, repo, ref, path] = match;
+
+  const res = await fetch(
+    `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${
+      ref || 'master'
+    }`
+  );
+  const json = await res.json();
+  return atob(json.content);
 }
 
 function getGistId(_url) {
@@ -19,7 +40,7 @@ function getGistId(_url) {
 
   let value = gist || url || id;
 
-  if (value.includes('github')) {
+  if (value.includes('gist.github')) {
     value = value.split('/').pop();
   }
 
