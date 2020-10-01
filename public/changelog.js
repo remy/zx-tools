d2e0491ada269652c22ab315fe577fe08d1d0ed5
Remy Sharp <remy@remysharp.com>
2020-10-01 13:55:50 +0100

    add change log

--- public/changelog.js
+++ public/changelog.js
@@ -1,11 +1,9 @@
-export default {};
-
-const upcoming = {
-  '945cc12fc0570a062d6ecbff6e5e654da1704303': {
+export default {
+  '69ec9cd9a15d541899ac18b4bd7f2e24cfe5c848': {
     date: '2020-10-01',
     title: 'New features',
     summary:
-      'New export controls. Repeat import patterns. Import palettes. 4 bit support. Animation previews and more: <a target="_blank" href="#">see a video of the features</a>',
+      'New export controls. Repeat import patterns. Import palettes. 4 bit support. Animation previews and more: <a target="_blank" href="https://youtu.be/lHte3oN5xsw">see a video of the features</a>',
     detail: '',
   },
 };
